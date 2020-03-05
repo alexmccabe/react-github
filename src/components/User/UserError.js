a5f20423ba7526f58c4705dd990fff21b477c097
Alex McCabe <alexwmccabe@gmail.com>
2020-03-05 10:07:30 +0000

    Fix user error

--- src/components/User/UserError.js
+++ src/components/User/UserError.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function UserError(props) {
-  return <div>{props.error}</div>;
+  return (
+    <article class="message is-danger">
+      <div class="message-body">Error fetching user!</div>
+    </article>
+  );
 }
 
 export default UserError;
